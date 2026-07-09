16351ff26133f19f688c17413390da3e53b22c2c
Jongeum-k <k.jongeum@gmail.com>
2026-07-09 18:54:15 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,30 +4,35 @@ from sqlalchemy import text
 from sqlalchemy.orm import Session
 
 from app.db import get_db
-from app.api.admin_jobs import router as admin_router
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-app.include_router(admin_router)
 
 @app.get("/")
 def read_root():
     return {"message": "Howdy, cowboy. FastAPI is up and runnin"}
 
+
+
 @app.get("/health/db")
 def check_db(db: Session = Depends(get_db)):
-    result = db.execute(text("SELECT 1 AS ok"))
-    row = result.fetchone()
-
-    return {
-        "status": "ok",
-        "database": "connected",
-        "data": row.ok,
-    }
+    try:
+        result = db.execute(text("SELECT 1 AS ok"))
+        row = result.fetchone()
+        return {
+            "status": "ok",
+            "database": "connected",
+            "data": row.ok,
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Database connection failed: {str(e)}",
+        )
